0618707ce207f00c385f8368b8cb1bd0c40962f1
Isreallvs <23327669.angulo@ms.uas.edu.mx>
2026-09-22 11:01:54 -0700

    Corregi el texto nuevamente

--- Mini proyecto/C#/JuegoDamas/JuegoDamas.cs
+++ Mini proyecto/C#/JuegoDamas/JuegoDamas.cs
@@ -75,7 +75,7 @@ else if (opcionMenu == "2")
 
     if (!File.Exists(rutaPartida)) //revisa si realmente existe el archivo que pidio el jugador
     {
-        Console.WriteLine("No se encontro esa partida guardada (no uses espacios).");
+        Console.WriteLine("No se encontro esa partida guardada.");
         return;
     }
 
