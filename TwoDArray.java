6b7d6fbd2d68bd0ec10c56add4ed6d9c0cf78c3b
Isreallvs <23327669.angulo@ms.uas.edu.mx>
2026-09-26 00:24:21 -0700

    Agregue la matriz de 2d en java

---
+++ TwoDArray.java
@@ -0,0 +1,18 @@
+public class TwoDArray {
+    public static void main (String[] args){
+        int[][] TwoDimensionalArray = {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        };
+
+        System.out.println("Los elementos del array son: ");
+        for (int[] row : TwoDimensionalArray){
+            for (int element : row){
+                System.out.print(element + " ");
+
+            }
+            System.out.println();
+        }
+    }
+}
