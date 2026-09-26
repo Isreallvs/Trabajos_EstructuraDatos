25c22966ac158567eb9c7750879ab9e908e5d39d
Isreallvs <23327669.angulo@ms.uas.edu.mx>
2026-09-25 22:41:13 -0700

    Agregue el ordenamiento por seleccion en java

---
+++ SelectionSort.java
@@ -0,0 +1,40 @@
+public class SelectionSort {
+    static void selection (int[] a)
+    {
+        for (int i = 0; i < a.length; i++)
+        { // recorre todo el arreglo
+            int small = i; // índice del elemento más pequeño
+            for (int j = i + 1; j < a.length; j++) 
+            { // encuentra el elemento más pequeño en el arreglo
+                if (a[small] > a[j]) 
+                { // compara el elemento más pequeño con el siguiente elemento
+                    small = j; // actualiza el índice del elemento más pequeño
+                }
+                // intercambia el elemento más pequeño con el primer elemento
+                int temp = a[i];
+                a[i] = a[small];
+                a[small] = temp; // intercambia los elementos
+            }
+        }
+    }
+
+    static void printArr (int[] a)
+    {
+        for (int i : a)
+                System.out.print(i + " ");
+    }
+
+    public static void main(String[] args)
+    {
+        int[] a = {65, 26, 13, 23, 12};
+
+        System.out.println("Arreglo antes de ser ordenado: ");
+        printArr(a);
+
+        selection(a);
+
+        System.out.println("\nArreglo despues de ser ordenado: ");
+        selection(a);
+        printArr(a);
+    }
+}
