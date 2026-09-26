830268bef97e4d346634fc02e2ca42610cd73607
Isreallvs <23327669.angulo@ms.uas.edu.mx>
2026-09-26 00:31:18 -0700

    Agregue la matriz en 3d en c++

---
+++ matriz 3d.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int ThreeDimensionalArray[2][3][3] = {
+        {
+            {1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}
+        },
+        {
+            {10, 11, 12},
+            {13, 14, 15},
+            {16, 17, 18}
+        }
+    };
+
+    cout << "Los elementos del array son: " << endl;
+    for (int i = 0; i < 2; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                cout << ThreeDimensionalArray[i][j][k] << " "; // mostrar los elementos de la fila separados por espacios
+            }
+        }
+        cout << endl; // ir a la siguiente línea después de mostrar un bloque 2D
+    }
+
+    return 0;
+}
